17d9bd7a6d618e667356cec1c94885c463c9b765
NokuthabaSiphambili <nokuthaba2608@gmail.com>
2023-06-08 13:42:12 +0200

    Updated interest html,css and js files

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function photography() {
+  let firstName = prompt("What is your name?");
+  let lastName = prompt("What is your surname?");
+  let email = prompt("What is your email address?");
 
+  if (firstName.length) {
+    alert(
+      "Thank you, " +
+        firstName +
+        " " +
+        lastName +
+        "." +
+        " We'll chat soon via email🤗!"
+    );
+  } else {
+    alert("Sorry, we can't contact you🫤!");
+  }
+}
+
+let photographyButton = document.querySelector("button");
+photographyButton.addEventListener("click", photography);
